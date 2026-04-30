c8d354ab8dfa2926267dcdcb770b13641aa4c412
nourabosen <44316424+nourabosen@users.noreply.github.com>
2026-05-01 02:14:02 +0300

    console log updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const fetchMovies = async () => {
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            
+            const response = await fetch(endpoint, API_OPTIONS)
+
+            
+            if(!response.ok) {
+                throw new Error("Faield to fetch the movies. Told ya be smarter")
+            }
+            const data = await response.json()
+            console.log(data);
+        }
+        catch (error){
+            console.error(`Error fetching movies: ${error}`);
+            setErrorMessage('Error fetching movies. Be smarter');
+        }
+        
+    }
+    
+    useEffect(()=>{
+        fetchMovies();
+    }, []);
+
     return (
         <main>
-            <di className = "pattern"/>
+            <div className = "pattern"/>
             <div className = "wrapper">
                 <header>
-                    <img src= "./hero.png"/>
+                    <img src= "./hero.png" alt ="Hero Banner!"/>
                     <h1>
                         Find <span className = "text-gradient">Movies</span> You'll Enjoy
                     </h1>
-                </header>
-                <p>
                     <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />
-                    <br></br>
+                    <br/>
                     <h1 className="text-white">{searchTerm} </h1>
-                </p>
+                </header>
+                
+                <section className='all-movies'>
+                    <h2>All Movies</h2>
+                    {errorMessage && <p className="text-red-500"> {errorMessage}</p>}
+                </section>
             </div>
         </main>
   )
