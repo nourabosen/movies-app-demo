d5c1a3957c0abb481c997f9e9d6c7bf351be8cdf
nourabosen <44316424+nourabosen@users.noreply.github.com>
2026-05-01 18:19:14 +0300

    optimized search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import Search from './components/Search'
-import Spinner from './components/Spinner'
+import React, { useState, useEffect } from 'react';
+import Search from './components/Search';
+import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import {useDebounce} from 'react-use';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,6 +19,9 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState("")
     const [movieList, setMovieList] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+    useDebounce(()=> setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
     const fetchMovies = async (query='') => {
         setIsLoading(true);
@@ -50,8 +54,8 @@ const App = () => {
     }
     
     useEffect(()=>{
-        fetchMovies(searchTerm);
-    }, [searchTerm]);
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
 
     return (
         <main>
